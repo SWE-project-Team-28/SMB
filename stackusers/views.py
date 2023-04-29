6abf2908187644f15bbd2cda805be8ab58e0858c
tejaswis1508 <inboxofteju@gmail.com>
2023-04-29 18:41:55 +0530

    Update views.py

--- stackusers/views.py
+++ stackusers/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import ProfileUpdateForm, UserUpdateForm
 
+# Create your views here.
 def register(request):
+
+    form = UserCreationForm()
+    
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
