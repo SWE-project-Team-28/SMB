3634622b0a3cf54ea963812322342da166379357
tejaswis1508 <inboxofteju@gmail.com>
2023-05-01 04:38:08 +0530

    Update views.py

--- startprj/stackbase/views.py
+++ startprj/stackbase/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.db import models
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from .models import Question, Comment , Like
+from .forms import CommentForm
+from django.urls import reverse, reverse_lazy
 from taggit.models import Tag
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_protect
 
 
+def home(request):
+    return render(request, 'home.html')
 
 # Create your views here.
 
@@ -27,4 +40,66 @@ class QuestionListByTagView(ListView):
         context['tag_name'] = self.kwargs['tag_name']
         return context
 
-      
+class QuestionUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+    model = Question
+    fields = ['title', 'content','tag']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        questions = self.get_object()
+        if self.request.user == questions.user:
+            return True
+        return False
+
+class QuestionDeleteView(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
+    model = Question
+    context_object_name =  'question'
+    success_url = "/"
+
+    def test_func(self):
+        questions = self.get_object()
+        if self.request.user == questions.user:
+            return True
+        return False
+
+class CommentDetailView(CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'stackbase/question-detail.html'
+    
+    def form_valid(self, form):
+        form.instance.question_id = self.kwargs['pk']
+        return super().form_valid(form)
+    success_url = reverse_lazy('stackbase:question-detail')
+
+class AddCommentView(CreateView):
+    model = Comment
+    form_class = CommentForm
+    
+    template_name = 'stackbase/question-answer.html'
+
+    def form_valid(self, form):
+        form.instance.question_id = self.kwargs['pk']
+        return super().form_valid(form)
+    success_url = reverse_lazy('stackbase:question-lists')    
+     
+@login_required
+@csrf_protect
+
+def like_comment(request):
+    if request.method == 'POST':
+        comment_id = request.POST.get('comment_id')
+        is_like = request.POST.get('is_like')
+        comment = get_object_or_404(Comment, pk=comment_id)
+        user = request.user
+        if is_like == 'true':
+            Like.objects.create(user=user, comment=comment, is_like=True)
+        else:
+            Like.objects.create(user=user, comment=comment, is_like=False)
+        likes_count = Like.objects.filter(comment=comment).count()
+        return JsonResponse({'success': True, 'likes_count': likes_count})
+    else:
+        return JsonResponse({'success': False})
