8f128148e307861a2ba61b824eed4527356a22a3
sreeya1302 <108352092+sreeya1302@users.noreply.github.com>
2023-05-01 03:53:46 +0530

    Update urls.py

--- startprj/stackbase/urls.py
+++ startprj/stackbase/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     # CRUD Function
     # path('questions/', views.QuestionListView.as_view(), name="question-lists"),
     path('questions/new/', views.QuestionCreateView.as_view(), name="question-create"),
+    
+    path('questions/<int:pk>/<str:tag_name>/',views.QuestionListByTagView.as_view(),name="question-tag"),
+    path('tags/<str:tag_name>/',views.QuestionListByTagView.as_view(),name="tag-question"),
+    path('tags/', views.TagListView.as_view(), name='tag-list'),
 
 ]
