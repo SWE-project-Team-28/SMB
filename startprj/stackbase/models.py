84a06c40e2152ba17198613a12cb3e9fb94f7b33
shashidharrao19 <84169765+shashidharrao19@users.noreply.github.com>
2023-05-01 02:10:49 +0530

    Update models.py

--- startprj/stackbase/models.py
+++ startprj/stackbase/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=10000)
+    # content = models.TextField(null=True, blank=True)
+    content = RichTextUploadingField()
+    # likes = models.ManyToManyField(User, related_name='question_post')
+    date_created = models.DateTimeField(default=timezone.now)   
+    def __str__(self):
+        return f'{self.user.username} - Question'
+    
+    def get_absolute_url(self):
+        return reverse('stackbase:question-detail', kwargs={'pk':self.pk})
+    
+    
