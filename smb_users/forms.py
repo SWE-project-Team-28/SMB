4df0e7f3366837fe7bfd86d912c034a3e3af6769
ketan3007 <cs20btech11043@iith.ac.in>
2023-05-06 18:45:06 +0530

    forms.py done which has profile update form

--- smb_users/forms.py
+++ smb_users/forms.py
@@ -4,3 +4,34 @@ from django.contrib.auth.forms import UserCreationForm
 from django.db import models
 from django.db.models import fields
 from .models import Profile
+
+class Registeration_Form(UserCreationForm):
+    email = models.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+    def __init__(self, *args, **kwargs):
+        super(Registeration_Form, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'placeholder': 'Username'})
+        self.fields['email'].widget.attrs.update({'placeholder': 'Email Address'})
+        self.fields['password1'].widget.attrs.update({'placeholder': 'Password'})
+        self.fields['password2'].widget.attrs.update({'placeholder': 'Re-enter Password'})
+        self.order_fields(['username', 'email','password1', 'password2' ])
+
+class Profile_Update_Form(forms.ModelForm):
+    email = forms.EmailField()
+
+    def __init__(self, *args, **kwargs):
+        super(Profile_Update_Form, self).__init__(*args, **kwargs)
+        self.fields['username'] = forms.CharField(initial=self.instance.user.username, disabled= False)
+        self.fields['email'].widget.attrs.update({'placeholder': 'Email Address'})
+        self.fields['phone'].widget.attrs.update({'placeholder': 'Phone Number'})
+        self.fields['occupation'].widget.attrs.update({'placeholder': 'Occupation'})
+        self.fields['image'].widget.attrs.update({'class': 'custom-file-input'})
+        self.order_fields(['username', 'email', 'phone', 'occupation', 'image'])
+    class Meta:
+        model = Profile 
+        fields = ['email', 'phone', 'occupation','image']
+        
+    
