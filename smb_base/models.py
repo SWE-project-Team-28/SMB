7b7a05e396ba90c537fbe8e2bc69d73b05730d5d
tejaswis1508 <inboxofteju@gmail.com>
2023-05-06 18:20:05 +0530

    Update models.py

--- smb_base/models.py
+++ smb_base/models.py
@@ -1,1 +1,8 @@
-
+from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.conf import settings
+from taggit.models import Tag
