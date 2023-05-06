b6b0fee58549b51fe96a1f36b6d86269ba55ba2b
tejaswis1508 <inboxofteju@gmail.com>
2023-05-06 17:52:46 +0530

    Delete stackusers directory

--- stackusers/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class StackusersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'stackusers'
-
-    def ready(self):
-        import stackusers.signals
