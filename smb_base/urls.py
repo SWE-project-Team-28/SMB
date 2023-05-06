e93cd4063d4c9f029aaec14bfb4745882fb8062c
shashidharrao19 <84169765+shashidharrao19@users.noreply.github.com>
2023-05-06 22:22:17 +0530

    Update urls.py

--- smb_base/urls.py
+++ smb_base/urls.py
@@ -1,1 +1,18 @@
+from django.urls import path
+from . import views
 
+app_name = 'smb_base'
+
+urlpatterns = [
+    path('', views.Question_List_View.as_view(), name="question-lists"),
+    
+    path('questions/new/', views.Question_Create_View.as_view(), name="question-create"),
+    path('questions/<int:pk>/', views.Question_Detail_View.as_view(), name="question-detail"),
+    path('questions/<int:pk>/update/', views.Question_Update_View.as_view(), name="question-update"),
+    path('questions/<int:pk>/delete/', views.Question_Delete_View.as_view(), name="question-delete"),
+    path('questions/<int:pk>/comment/', views.Question_Comment_View.as_view(), name="question-comment"),
+    path('questions/<int:pk>/<str:tag_name>/',views.Question_List_By_Tag_View.as_view(),name="question-tag"),
+    path('tags/<str:tag_name>/',views.Question_List_By_Tag_View.as_view(),name="tag-question"),
+    path('tags/', views.Tag_List_View.as_view(), name='tag-list'),
+    path('like/<int:pk>', views.comment_like, name="like_post")
+]
