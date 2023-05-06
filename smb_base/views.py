2f183c1f5120d71b9df095e4842a5b4bfc18b940
tejaswis1508 <inboxofteju@gmail.com>
2023-05-06 18:38:55 +0530

    Update views.py

--- smb_base/views.py
+++ smb_base/views.py
@@ -13,3 +13,32 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_protect
 from django.db.models import Q
 from django.utils.decorators import method_decorator
+
+
+class Question_Detail_View(DetailView):
+
+    model = Question
+
+
+      
+@method_decorator(login_required, name='dispatch')
+class Question_Comment_View(CreateView):
+    """
+    View for adding a new comment to a question.
+    """
+    template_name = 'smb_base/question-answer.html'
+    model = Comment
+    form_class = Answer_Form
+
+    def form_valid(self, form):
+        """
+        If the form is valid, set the question ID for the new comment and save it.
+        """
+        form.instance.question_id = self.kwargs['pk']
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        """
+        Returns the URL to redirect to after a successful form submission.
+        """
+        return reverse_lazy('smb_base:question-lists')
