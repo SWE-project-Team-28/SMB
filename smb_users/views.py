5dda313c5d2e0f0e8d264bfa4a406e69bf0d9234
tejaswis1508 <inboxofteju@gmail.com>
2023-05-06 18:27:47 +0530

    Update views.py

--- smb_users/views.py
+++ smb_users/views.py
@@ -1,1 +1,8 @@
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import Profile_Update_Form, Registeration_Form
+from smb_base.models import Question
+from django.shortcuts import render, get_object_or_404
+import sweetify
