3cf4365efe1805633657590788492614dc42f964
tejaswis1508 <inboxofteju@gmail.com>
2023-05-06 18:21:11 +0530

    Update views.py

--- smb_base/views.py
+++ smb_base/views.py
@@ -1,1 +1,15 @@
-
+from django.db import models
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from .models import Question, Comment 
+from .forms import Answer_Form
+from django.urls import reverse, reverse_lazy
+from taggit.models import Tag
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_protect
+from django.db.models import Q
+from django.utils.decorators import method_decorator
