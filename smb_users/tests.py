26055d16996a5a94575bf03204a703cf82d5d56d
ketan3007 <cs20btech11043@iith.ac.in>
2023-05-06 18:40:36 +0530

    tests case of ask ques and view profile added

--- smb_users/tests.py
+++ smb_users/tests.py
@@ -20,3 +20,25 @@ class UserSignInTestCase(TestCase):
         #self.assertTrue(user_exists)
 
 
+class ProfilePageTestCase(TestCase):
+
+    def test_user_sign_in(self):
+        # Make a POST request to the sign in page with user details
+        response = self.client.post('/profile', {
+            'username': 'testuser',
+            'email': 'testuser@iith.ac.in',
+            'password1': 'testpass',
+            'password2': 'testpass',
+        })
+
+        # Check if the user is redirected to the home page after successful sign in
+        self.assertRedirects(response, '/profile')
+
+
+    def test_ask_question(self):
+        response = self.client.get(reverse('profile'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '/questions/new')
+
+
+        
