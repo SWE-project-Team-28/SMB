9b352bd641acf908c5ba703e5cb12270a25b372b
tejaswis1508 <inboxofteju@gmail.com>
2023-05-06 18:25:23 +0530

    Update apps.py

--- smb_users/apps.py
+++ smb_users/apps.py
@@ -1,1 +1,9 @@
+from django.apps import AppConfig
 
+
+class smb_usersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'smb_users'
+
+    def ready(self):
+        import smb_users.signals
