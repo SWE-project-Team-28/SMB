1ee3260a315af1220337074298529525488b486b
ketan3007 <cs20btech11043@iith.ac.in>
2023-05-06 18:39:10 +0530

    user sign in test case added

--- smb_users/tests.py
+++ smb_users/tests.py
@@ -1,1 +1,22 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+class UserSignInTestCase(TestCase):
+    def test_user_sign_in(self):
+        # Make a POST request to the sign in page with user details
+        response = self.client.post('/', {
+            'username': 'testuser',
+            'email': 'testuser@iith.ac.in',
+            'password1': 'testpass',
+            'password2': 'testpass',
+        })
+
+        # Check if the user is redirected to the home page after successful sign in
+        self.assertRedirects(response, '/')
+
+        # Check if the user is created in the database
+        #user_exists = User.objects.filter(username='testuser').exists()
+        #self.assertTrue(user_exists)
+
 
