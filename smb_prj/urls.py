e7d792713553c0a9f9ea57989f647f4c6b86a4ff
shashidharrao19 <84169765+shashidharrao19@users.noreply.github.com>
2023-05-07 11:19:15 +0530

    Update urls.py

--- smb_prj/urls.py
+++ smb_prj/urls.py
@@ -7,7 +7,19 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('smb_base.urls')),
+    path('ckeditor',include('ckeditor_uploader.urls')),
+    # Authentication System
+    path('register/', user_view.register, name="register"),
+    path('login/', auth_view.LoginView.as_view(template_name="smb_users/login.html"), name='login'),
+    path('logout/', auth_view.LogoutView.as_view(template_name="smb_users/logout.html"), name='logout'),
     
+    path('profile/', user_view.profile, name="profile"),
+    path('profile/update/', user_view.profile_update, name="profile_update"),
+    path('profile/questions/', user_view.asked_by_user, name="question_user") 
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
